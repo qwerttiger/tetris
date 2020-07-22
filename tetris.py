92fce2abe987df23dc27ea5bb832fc35dc2bd6fa
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-07-22 12:18:34 -0400

    i gotta eat lunch and i changed name and added fill screen

--- tetris.py
+++ tetris.py
@@ -2,3 +2,6 @@ import pygame #import pygame
 pygame.init() #initiate pygame
 screen=pygame.display.set_mode((700,700)) #setup display
 pygame.display.set_caption("Tetris") #set caption
+tetrislogo=pygame.image.load("C:/Users/Rainbow/Documents/GitHub/tetris/pictures/tetrislogo.png")
+screen.fill((50,50,50))
+pygame.display.flip()
