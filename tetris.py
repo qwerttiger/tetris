20869cde78601adb460267741b6d3350fc096933
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-07-23 12:19:29 -0400

    add blocks (turned) and added loading files

--- tetris.py
+++ tetris.py
@@ -1,3 +1,4 @@
+import os #import os, a tool for finding files and
 import pygame,sys #import pygame and sys, a tool for exiting
 pygame.init() #initiate pygame
 screen=pygame.display.set_mode((700,700)) #setup display
@@ -8,6 +9,19 @@ screen.fill((50,50,50)) #fill screen
 screen.blit(tetrislogo,(350-tetrislogo.get_width()/2,100-tetrislogo.get_height()/2)) #draw the tetris logo
 screen.blit(play,((350-play.get_width()/2,600-play.get_height()/2))) #draw the play button
 keep_going=True #set keep going to True
+def load(names):
+  for name in names:
+    print(name)
+    name_=f"C:/Users/Rainbow/Documents/GitHub/tetris/pictures/{name}.png"
+    exec(f"global {name}")
+    exec(f"{name}=pygame.image.load('{name_}')",globals())
+listofblocks=[]
+for root,_,files in os.walk("C:/Users/Rainbow/Documents/GitHub/tetris/pictures"):
+  for file in files:
+    if file not in ["play.png","tetrislogo.png"]:
+      print(root,file[:-4])
+      listofblocks+=[file[:-4]]
+load(listofblocks)
 while keep_going: #while you are keep going
   for event in pygame.event.get(): #for every event
     if event.type==pygame.QUIT: #if the type is quit
