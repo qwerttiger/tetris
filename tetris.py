97343c5607269d028831d372401d2a5ec64332e7
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-07-24 17:04:04 -0400

    okay it can do stuff now... kinda

--- tetris.py
+++ tetris.py
@@ -1,6 +1,6 @@
 import os #import os, a tool for finding files and not letting python write support prompt
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT']="hide" #hide support prompt
-import pygame,sys,random #import pygame and sys, a tool for exiting, and random, a tool for random choices
+import pygame,sys,random,threading #import pygame and sys, a tool for exiting, and random, a tool for random choices, and threading, a tool for setting non-blocking timers
 pygame.init() #initiate pygame
 screen=pygame.display.set_mode((700,700)) #setup display
 pygame.display.set_caption("Tetris") #set caption
@@ -17,9 +17,12 @@ def load(names):
     exec(f"global {name}")
     exec(f"{name}=pygame.image.load('{name_}')",globals())
 def randomblock():
-  return random.choice("O left right tleft tright I tI L lL fL rL oL loL foL roL t rt ft lt".split())
+  x=random.choice("O left right tleft tright I tI L lL fL rL oL loL foL roL t rt ft lt".split())
+  a=eval(x)
+  a.set_colorkey((255,255,255))
+  return a,x
 class matrix:
-  def __init__(self,string):
+  def __init__(self,string=(("0"*10+"\n")*20)[:-1]):
     self.matrix=[[int(y) for y in x] for x in string.split("\n")]
     self.x=len(self.matrix[0])
     self.y=len(self.matrix)
@@ -29,8 +32,8 @@ class matrix:
     except:
       return matrix("")
   def collide(self,other,diff):
-    for ypos1 in range(20):
-      for xpos1 in range(10):
+    for ypos1 in range(20-other.y):
+      for xpos1 in range(10-other.x):
         xpos2=xpos1+diff[0]
         ypos2=ypos1+diff[1]
         if self.getat(xpos1,ypos1)==other.getat(xpos2,ypos2):
@@ -38,6 +41,22 @@ class matrix:
     return False
   def clear(self):
     self.matrix=[[0]*len(self.matrix[0])]*len(self.matrix)
+def draw(blocks):
+  a=blocks[0][0]
+  b=blocks[1][0]
+  c=blocks[2][0]
+  d=blocks[3][0]
+  nextsurface.blit(a,(50-a.get_width()/2,50-a.get_height()/2))
+  nextsurface.blit(b,(50-b.get_width()/2,150-b.get_height()/2))
+  nextsurface.blit(c,(50-c.get_width()/2,250-c.get_height()/2))
+  nextsurface.blit(d,(50-d.get_width()/2,350-d.get_height()/2))
+def getmatrix(block):
+  block=block[1]
+  def cond(block,listt):
+    for x in listt:
+      if block==x[0]:
+        return matrix(x[1])
+  return cond(block,(("O","11\n11"),("left","10\n11\n01"),("right","01\n11\n10"),("tleft","011\n110"),("tright","110\n011"),("I","1\n1\n1\n1"),("tI","1111"),("L","10\n10\n11"),("rL","111\n100"),("fL","11\n01\n01"),("lL","001\n111"),("oL","01\n01\n11"),("roL","100\n111"),("foL","11\n10\n10"),("loL","111\n001"),("t","111\n010"),("rt","01\n11\n01"),("ft","010\n111"),("lt","10\n11\n10")))
 listofblocks=[]
 for root,_,files in os.walk("C:/Users/Rainbow/Documents/GitHub/tetris/pictures"):
   for file in files:
@@ -57,8 +76,30 @@ while keep_going: #while you are keep going
 screen.fill((50,50,50))
 pygame.display.flip()
 keep_going=True
-screenmatrix=matrix("")
-screenmatrix.clear()
+screenmatrix=matrix()
+blocks=[randomblock(),randomblock(),randomblock(),randomblock()]
+currentblock=randomblock()
+nextsurface=pygame.Surface((100,400))
+matrixscreen=pygame.Surface((100,200))
+alreadymatrix=pygame.Surface((100,200))
 while keep_going:
-  for event in pygame.event.get():
-    pass
+  screen.fill((50,50,50))
+  nextsurface.fill((100,100,100))
+  matrixscreen.fill((255,255,255))
+  alreadymatrix.fill((255,255,255))
+  currentblock=blocks.pop(0)
+  blocks+=[randomblock()]
+  draw(blocks)
+  screen.blit(nextsurface,(550,50))
+  screen.blit(matrixscreen,(300,250))
+  blockmatrix=getmatrix(currentblock)
+  xpos=random.randint(0,currentblock[0].get_width()/10-1)*10
+  ypos=0
+  while True:
+    matrixscreen.fill((255,255,255))
+    matrixscreen.blit(currentblock[0],(xpos,round(ypos/10)*10))
+    screen.blit(matrixscreen,(300,250))
+    ypos+=0.01
+    pygame.display.flip()
+    for event in pygame.event.get():
+      pass
